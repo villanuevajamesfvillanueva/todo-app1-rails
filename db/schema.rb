d5e8806385d22bd6edbac465d9401333a164c968
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-05-29 10:23:16 +0800

    added new model category

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_29_021606) do
+ActiveRecord::Schema.define(version: 2021_05_29_022137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
