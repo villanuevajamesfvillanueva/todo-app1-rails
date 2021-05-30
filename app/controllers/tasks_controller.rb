23b355e8af431e8efb7bacb49957b6eea01b95ef
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-05-31 02:04:39 +0800

    added CRUD for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,50 @@
 class TasksController < ApplicationController
     def index
-        @tasks = Task.all
+        @tasks = Task.all.order("updated_at DESC")
     end
+
+    def new                         
+        @task = Task.new
+    end
+
+    def show
+        @task = Task.find(params[:id])
+    end
+
+    def create
+        @task = Task.new(task_params)
+
+        if @task.valid?
+            @task.save
+            redirect_to '/' 
+            
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @task = Task.find(params[:id])
+    end
+
+    def update
+        @task = Task.find(params[:id])
+        if @task.update(task_params)
+            redirect_to '/'
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        redirect_to '/'
+    end
+
+    private
+    def task_params
+        params.require(:task).permit(:title, :body)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'tasks#index'
+
+  put '/tasks/:id/edit', to: 'tasks#update', as: 'tasks_update'
+  post '/tasks' => 'tasks#create', as: 'task_create'
   resources :tasks
 end
