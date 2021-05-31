5299b247290610d4ce9746e90df574ed8e5c7392
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-06-01 01:39:32 +0800

    added CRUD to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,4 +6,48 @@ class CategoriesController < ApplicationController
     def new                        
         @category = Category.new
     end
+
+    
+    #for destroying categories, corresponding fk to tasks tables should be deleted
+    def show
+        @category = Category.find(params[:id])
+    end
+
+    def create
+        @category = Category.new(category_params)
+
+        if @category.valid?
+            @category.save
+            redirect_to '/' 
+            
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @category = Category.find(params[:id])
+    end
+
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            redirect_to '/'
+        else
+            render :edit
+        end
+    end
+
+    
+    def destroy
+        @category = Category.find(params[:id])
+        @category.destroy
+        redirect_to '/'
+    end
+    
+
+    private
+    def category_params
+        params.require(:category).permit(:name)
+    end
 end
