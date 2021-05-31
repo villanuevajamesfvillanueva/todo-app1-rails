45e26776260a63c6b214a3d53b8a4e6381333f6c
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-05-31 21:00:21 +0800

    added model test for tasks

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -4,4 +4,8 @@ class TaskTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  # test 'should not save task without title' do
+  #   task = Task.new
+  #   assert_not task.save, 'Error! Task saved without title'
 end
