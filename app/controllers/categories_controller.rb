23b15e742bfad52cc0c78dfedbf68d9563937a09
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-06-05 04:17:05 +0800

    added user_id in params; added before_action in controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -48,6 +48,6 @@ class CategoriesController < ApplicationController
 
     private
     def category_params
-        params.require(:category).permit(:name)
+        params.require(:category).permit(:name, :user_id)
     end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+
+    skip_before_action :authenticate_user!, :only => [:index]
+    
     def index
         @tasks = Task.all.order("updated_at DESC")
         @categories = Category.all.order("updated_at DESC")
