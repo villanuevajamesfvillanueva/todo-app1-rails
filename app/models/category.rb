6a231c2db158901efd186b5b0c496c7b2735a5fd
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-06-05 11:24:20 +0800

    added uniqueness scoped to user for tasks and categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
     belongs_to :user
     has_many :tasks, dependent: :nullify
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: {scope: :user_id}
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,26 @@
 class Task < ApplicationRecord
     belongs_to :category, optional: true
 
-    validates :title, presence: true, uniqueness: true
+    validate :uniqueness_to_user
+    validates :title, presence: true
     validates :body, presence: true
     validates :deadline, presence: true
+
+    def uniqueness_to_user 
+        sql = <<~SQL
+            SELECT *
+            FROM tasks, categories, users 
+            WHERE categories.user_id = '#{User.current.id}' AND tasks.category_id = categories.id
+            AND tasks.title = '#{title}'
+        SQL
+
+        records = ActiveRecord::Base.connection.exec_query(sql).to_a
+        if (records.present?)
+            errors.add(:title, "already exists")    
+        end
+    end
+
+    
+    
+
 end
